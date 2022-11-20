ba2261c6b90f5c682bbb0b1f518b4d5d8fac8044
kim dawoon <dw.kim1@greenlabs.co.kr>
2022-11-20 21:59:16 +0900

    form-design
    Signed-off-by: kim dawoon <dw.kim1@greenlabs.co.kr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./App.css";
 
+const TextInput = () => (
+  <input type="text" className="border p-2 rounded outline-none w-full" />
+);
+
 const App = () => {
   return (
     <div className="App">
-      <h1 className="bg-green-200">test</h1>
+      <div className="h-screen p-[10%] py-[20%] bg-gray-100">
+        <div className="flex items-center justify-center flex-col gap-2">
+          <TextInput />
+          <TextInput />
+          <TextInput />
+          <input
+            type="submit"
+            className="border bg-blue-400 w-full"
+            value="submit"
+          />
+        </div>
+      </div>
     </div>
   );
 };
