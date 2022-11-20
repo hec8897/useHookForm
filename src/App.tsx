df9b589692f3185be646bafa1ba878f52a2acf67
김다운 <37240562+hec8897@users.noreply.github.com>
2022-11-20 22:42:26 +0900

    기본 적용 (#2)
    Signed-off-by: kim dawoon <dw.kim1@greenlabs.co.kr>
    
    Signed-off-by: kim dawoon <dw.kim1@greenlabs.co.kr>
    Co-authored-by: kim dawoon <dw.kim1@greenlabs.co.kr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from "react";
 import "./App.css";
-
-const TextInput = () => (
-  <input type="text" className="border p-2 rounded outline-none w-full" />
-);
+import { useForm } from "react-hook-form";
 
 const App = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      name: "다운",
+      gender: "male",
+    },
+  });
+
+  const onSubmit = async (data: any) => console.log(data);
+
   return (
     <div className="App">
       <div className="h-screen p-[10%] py-[20%] bg-gray-100">
-        <div className="flex items-center justify-center flex-col gap-2">
-          <TextInput />
-          <TextInput />
-          <TextInput />
-          <input
-            type="submit"
-            className="border bg-blue-400 w-full"
-            value="submit"
-          />
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex items-center justify-center flex-col gap-2">
+            <input
+              className="border w-full p-1"
+              type="text"
+              {...register("name")}
+            />
+            <select className="border w-full p-1" {...register("gender")}>
+              <option value="female">female</option>
+              <option value="male">male</option>
+            </select>
+            <input
+              type="submit"
+              className="border bg-blue-400 w-full"
+              value="submit"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
